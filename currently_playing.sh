5deca4bbd58f8f738a44095fe1562730e431fecf
Jakub Chrpa <diahexik@gmail.com>
2023-01-30 22:48:03 +0100

    Script: Fixed a bug where the script sometimes gave blank output.

--- currently_playing.sh
+++ currently_playing.sh
@@ -9,17 +9,20 @@ fi
 while true
 do
   API_DATA="`curl -X GET "https://api.spotify.com/v1/me/player/currently-playing" -H "Authorization: Bearer $APIKEY" --silent`" 
-  echo $API_DATA | grep -E "The access token expired" > /dev/null && echo "Api key expired, generate a new one." && exit 1  
-  echo $API_DATA | grep -E "Invalid access token" > /dev/null && echo "Invalid Api key (try generating a new one)." && exit 1  
-  echo "$API_DATA" | sed -n "2p" | grep -E "error" > /dev/null && echo "Unknown error (try generating a new api key)." && exit 1 
-  if [ "$API_DATA" = "" ];
+  echo $API_DATA | grep -E "The access token expired" > /dev/null && echo "Api key expired, generate a new one." && exit 1
+  echo $API_DATA | grep -E "Invalid access token" > /dev/null && echo "Invalid Api key (try generating a new one)." && exit 1
+  echo $API_DATA | grep -E "Service unavailable" > /dev/null && echo "Api limit, please wait." && sleep 15
+  echo "$API_DATA" | sed -n "2p" | grep -E "error" && echo "$API_DATA" >> last_error && echo "Unknown error (try generating a new api key)."
+  if [ -z "$API_DATA" ];
   then
-    echo "No song playing." > current_song
+    echo "No song playing" > current_song
+    sleep 15
+    continue
   else
     NAMES=`echo "$API_DATA" | grep -E "name"`
-    ARTIST=`echo $NAMES | cut -d '"' -f4`
-    SONG=`echo $NAMES | cut -d '"' -f16`
-    echo "$ARTIST - $SONG" > current_song 
+    ARTIST=`echo $NAMES | cut -d ':' -f2 | cut -d '"' -f2`
+    SONG=`echo $NAMES | cut -d ':' -f5 | cut -d '"' -f2`
+    [ -n "$ARTIST" ] && [ -n "$SONG" ] && echo "$ARTIST - $SONG" > current_song
   fi
-  sleep 5
+  sleep 15
 done
