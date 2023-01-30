11d04733bdfb8b3f1763e95818d3556bbe47f7a1
Jakub Chrpa <diahexik@gmail.com>
2023-01-30 22:57:11 +0100

    Script: Added a continue command to get new data after getting api limitted.

--- currently_playing.sh
+++ currently_playing.sh
@@ -11,7 +11,7 @@ do
   API_DATA="`curl -X GET "https://api.spotify.com/v1/me/player/currently-playing" -H "Authorization: Bearer $APIKEY" --silent`" 
   echo $API_DATA | grep -E "The access token expired" > /dev/null && echo "Api key expired, generate a new one." && exit 1
   echo $API_DATA | grep -E "Invalid access token" > /dev/null && echo "Invalid Api key (try generating a new one)." && exit 1
-  echo $API_DATA | grep -E "Service unavailable" > /dev/null && echo "Api limit, please wait." && sleep 15
+  echo $API_DATA | grep -E "Service unavailable" > /dev/null && echo "Api limit, please wait." && sleep 15 && continue
   echo "$API_DATA" | sed -n "2p" | grep -E "error" && echo "$API_DATA" >> last_error && echo "Unknown error (try generating a new api key)."
   if [ -z "$API_DATA" ];
   then
