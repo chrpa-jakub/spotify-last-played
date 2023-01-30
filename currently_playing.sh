d51c632d31bb8f14d3bb731270963326ededf093
Jakub Chrpa <diahexik@gmail.com>
2023-01-30 21:06:52 +0100

    Script: Added a message if the given API key is expired.

--- currently_playing.sh
+++ currently_playing.sh
@@ -8,15 +8,16 @@ fi
 
 while true
 do
-API_DATA="`curl -X GET "https://api.spotify.com/v1/me/player/currently-playing" -H "Authorization: Bearer $APIKEY" --silent`" 
-if [ "$API_DATA" = "" ];
-then
-  echo "No song playing." > current_song
-else
-  NAMES=`echo "$API_DATA" | grep -E "name"`
-  ARTIST=`echo $NAMES | cut -d '"' -f4`
-  SONG=`echo $NAMES | cut -d '"' -f16`
-  echo "$ARTIST - $SONG" > current_song 
-fi
-sleep 5
+  API_DATA="`curl -X GET "https://api.spotify.com/v1/me/player/currently-playing" -H "Authorization: Bearer $APIKEY" --silent`" 
+  echo $API_DATA | grep -E "The access token expired" > /dev/null && echo "Api key expired, generate a new one." && exit 1  
+  if [ "$API_DATA" = "" ];
+  then
+    echo "No song playing." > current_song
+  else
+    NAMES=`echo "$API_DATA" | grep -E "name"`
+    ARTIST=`echo $NAMES | cut -d '"' -f4`
+    SONG=`echo $NAMES | cut -d '"' -f16`
+    echo "$ARTIST - $SONG" > current_song 
+  fi
+  sleep 5
 done
